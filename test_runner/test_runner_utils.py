e07ed1c951573e8adffd39074713e705eb92c87a
Inha Yavorska <Inga.Yavorskaya@gmail.com>
2022-09-28 23:55:36 +0200

    Fixed counting of passed/failed tests

--- test_runner/test_runner_utils.py
+++ test_runner/test_runner_utils.py
@@ -5,12 +5,8 @@ import pytest
 class ResultsCollector:
     def __init__(self):
         self.reports = []
-        self.collected = 0
-        self.exitcode = 0
         self.passed = 0
         self.failed = 0
-        self.xfailed = 0
-        self.skipped = 0
         self.total_duration = 0
 
     @pytest.hookimpl(hookwrapper=True)
@@ -20,15 +16,8 @@ class ResultsCollector:
         if report.when == 'call':
             self.reports.append(report)
 
-    def pytest_collection_modifyitems(self, items):
-        self.collected = len(items)
-
     def pytest_terminal_summary(self, terminalreporter, exitstatus):
-        print(exitstatus.__dir__)
-        self.exitcode = exitstatus
-        self.passed = len(terminalreporter.stats.get('passed', []))
-        self.failed = len(terminalreporter.stats.get('failed', []))
-        self.xfailed = len(terminalreporter.stats.get('xfailed', []))
-        self.skipped = len(terminalreporter.stats.get('skipped', []))
-
+        for report in self.reports:
+            self.passed += 1 if report.outcome == "passed" else 0
+            self.failed += 1 if report.outcome == "failed" else 0
         self.total_duration = time.time() - terminalreporter._sessionstarttime
